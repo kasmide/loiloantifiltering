6cd9e80cb1b0d907e85eeb6afd2d1aed58adb915
kasmide <kasmide@zohomail.com>
2020-02-08 20:41:41 +0900

    Remove anti brute-force attack function
    We don't need to afraid this app to be used as springboard since this app doesn't proxy any requests.

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ let port = 3000
 
 const http = require("http");
 const url = require("url");
-const request = require("request");
-let blockedAccounts = new Object();
 if (process.argv[2]) {
   if (!isNaN(process.argv[2])) {
     port = Number(process.argv[2])
@@ -14,62 +12,19 @@ if (process.argv[2]) {
 }
 
 http.createServer(function (req, res) {
-  realHost = req.headers.host
-  req.headers.host = url.parse(loiloAPIserver).host
   const requestURL = loiloAPIserver + url.parse(req.url).path.substring(url.parse(req.url).pathname.indexOf("/api"));
-  console.log(req.method + " " + requestURL)
-  const data = [];
-  req.on('data', function (chunk) {
-    data.push(chunk);
-  })
-  req.on('end', function () {
-    switch (url.parse(requestURL).pathname) {
-      case "/api/web_filtering":
-        res.end('{"type":"none"}')
-        return;
-      case "/api/web_card/browsing_status":
-        res.end("{}");
-        return;
-      case "/api/tokens":
-        const usernameArray = String(Buffer.concat(data)).match(/user=.[^&]*/g);
-        if (usernameArray) {
-          const username = usernameArray[usernameArray.length - 1].substring(5);
-          if (!blockedAccounts[username] || new Date().getTime() - blockedAccounts[username]["time"] >= 3600000 || !blockedAccounts[username]["blocked"]) {
-            request({ url: requestURL, method: req.method, body: Buffer.concat(data), headers: req.headers, encoding: null }, function (error, response, body) {
-              if (response.statusCode != 200) {
-                if (!blockedAccounts[username]) {
-                  blockedAccounts[username] = {};
-                  blockedAccounts[username]["failCount"] = 1;
-                  blockedAccounts[username]["time"] = new Date().getTime();
-                } else {
-                  if (new Date().getTime() - blockedAccounts[username]["time"] >= 3600000) {
-                    blockedAccounts[username]["failCount"] = 1;
-                    blockedAccounts[username]["time"] = new Date().getTime();
-                    delete blockedAccounts[username]["blocked"]
-                  } else {
-                    blockedAccounts[username]["failCount"]++;
-                    if (blockedAccounts[username]["failCount"] >= 10) {
-                      blockedAccounts[username]["blocked"] = true;
-                      blockedAccounts[username]["time"] = new Date().getTime();
-                    }
-                  }
-                }
-              }
-            }).pipe(res)
-          } else {
-            res.writeHead(401);
-            res.end("{\"status\":401}")
-          }
-          return;
-        }
-      default:
-        res.writeHead(308, {
-          "Location": requestURL
-        })
-        res.end();
-        return;
-    }
+  console.log("%s %s",req.method,requestURL)
+  switch (url.parse(requestURL).pathname) {
+    case "/api/web_filtering":
+      res.end('{"type":"none"}')
+      break;
+    case "/api/web_card/browsing_status":
+      res.end("{}");
+      break;
+    default:
+      res.writeHead(308, {"Location": requestURL})
+      res.end();
+      break;
   }
-  )
 }).listen(port);
 console.log("Listen on 0.0.0.0:" + port);
