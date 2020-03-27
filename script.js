446c2287a9220fc9b7cc5d91dd6471b299ce0a59
kasmide <kasmide@zohomail.com>
2020-03-27 18:53:14 +0900

    Animate various elements

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  this.scrolled();
+  document.addEventListener("scroll", scrolled, { passive: true });
+}
+async function scrolled() {
+  if (document.documentElement.scrollTop > 50)
+  {
+    document.getElementsByTagName("header")[0].style["animation-name"] = "hide"
+  } else {
+    document.getElementsByTagName("header")[0].style["animation-name"] = "show"
+  }
+  for (i = 0; document.getElementsByClassName("fade-in").length > i;i++){
+    if (document.getElementsByClassName("fade-in")[i].getBoundingClientRect().top < document.documentElement.scrollTop+window.innerHeight) {
+      await sleep(100);
+      document.getElementsByClassName("fade-in")[i].classList.add("shown");
+    };
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
